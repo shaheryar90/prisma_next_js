d70991cfb14da68c149eac7425fadd227c5a3f72
Shaheryar khan <58131449+shaheryar90@users.noreply.github.com>
2024-07-05 12:27:07 -0700

    project deploy

--- src/app/_lib/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function PrivateRoute() {
-  return <h2>you can't view this in the browser</h2>;
-}
-
-// _lib use for private folder with index.js and private folder itself. index.js will contain PrivateRoute component.
