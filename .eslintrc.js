8755c15c127e8ee2596188578b4252039ea22212
Shaheryar khan <58131449+shaheryar90@users.noreply.github.com>
2024-07-05 12:46:52 -0700

    project deploy

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@next/next/recommended",
+  ],
+  rules: {
+    // Additional rules or overrides can be added here
+  },
+};
